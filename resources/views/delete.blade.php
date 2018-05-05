2228040aa6fa3f4fca4297f0a97c37e517649539
Charlie Gao <gcte2010@gmail.com>
2018-05-04 20:36:03 -0400

    clean up

--- resources/views/delete.blade.php
+++ resources/views/delete.blade.php
@@ -24,7 +24,7 @@
     <div class="row">
         <div class="col-sm-offset-2 col-md-offset-2 col-lg-offset-3 col-sm-8 col-md-8 col-lg-6">
            @if (session('messages'))
-                <div class="alert alert-success">                   
+                <div class="alert alert-success">
                     @foreach (session('messages') as $message)
                         {{ $message }}
                     @endforeach
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,44 +18,42 @@
     <link rel='stylesheet' href='/css/post.css' type='text/css'>
 
     @stack('head')
-
-
 </head>
 <body>
     <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="/">Roomie Finder</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li><a href="/posts/">Browse</a></li>
-        <li><a href="/posts/create">Create</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<div class="container" style="margin-top:0px">
-    <div>
-        @yield('title')
-    </div>
-    
-    <div>
-        @yield('errors')
-    </div>
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>                        
+                </button>
+                <a class="navbar-brand" href="/">Roomie Finder</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="/posts/">Browse</a></li>
+                    <li><a href="/posts/create">Create</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container" style="margin-top:0px">
+        <div>
+            @yield('title')
+        </div>
+        
+        <div>
+            @yield('errors')
+        </div>
 
-    <div>
-        @yield('messages')
-    </div>
+        <div>
+            @yield('messages')
+        </div>
 
-    <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+        <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
 
-    @stack('body')
-</div><!-- /.container -->
+        @stack('body')
+    </div><!-- /.container -->
 </body>
 </html>
